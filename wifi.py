4930dcfd11a77a4064c2114693e022a6276aa22c
shivansh sharma <shivansh.sharma706@gmail.com>
2020-09-25 11:53:09 +0530

    First commit

---
+++ wifi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import subprocess
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+
+wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
+
+for wifi in wifis:
+	results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', wifi, 'key=clear']).decode('utf-8').split('\n')
+	results = [line.split(':')[1][1:-1] for line in results if "Key Content" in line]
+	
+	try:
+		print(f'Name: {wifi}, password: {results[0]}')
+	except IndexError:
+		print(f'Name: {wifi}, password: Cannot found!')
